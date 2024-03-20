4a91658a63b7e550b29bf8954af60b8b3e3b6f69
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-20 19:33:25 +0530

    code added

--- app.js
+++ app.js
@@ -21,20 +21,44 @@
 //     }
 // };
 
-let studentInfo = [
-    {
-        name: "Binay",
-        grade: "A+",
-        city: "Bhubaneswar"
-    },
-    {
-        name: "sradha",
-        grade: "D-",
-        city: "Delhi"
-    },
-    {
-        name: "Kunja",
-        grade: "A",
-        city: "Kendujhar"
+// let studentInfo = [
+//     {
+//         name: "Binay",
+//         grade: "A+",
+//         city: "Bhubaneswar"
+//     },
+//     {
+//         name: "sradha",
+//         grade: "D-",
+//         city: "Delhi"
+//     },
+//     {
+//         name: "Kunja",
+//         grade: "A",
+//         city: "Kendujhar"
+//     }
+// ];
+
+const max = prompt("Enter the maximum number");
+
+const random = Math.floor( Math.random() * max ) + 1;
+
+let guess = prompt("guess the number");
+
+while(true){
+    if(guess == "quit") {
+        console.log("you quit the game");
+        break;
     }
-];
+
+    if(guess == random) {
+        console.log("congrats you guess the right", random);
+        break;
+    }
+
+    else if(guess < random) {
+        guess = prompt("small number");
+    } else {
+        guess = prompt("larger number");
+    }
+}
